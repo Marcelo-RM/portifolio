dc39293e32280bb45856b2bf0462746a30e0af46
Marcelo-RM <36489721+Marcelo-RM@users.noreply.github.com>
2019-03-10 21:15:57 -0300

    iniciado header bar

--- controller/index.js
+++ controller/index.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
-	debugger;
 	$(".header").css('height', window.innerHeight);
+
+	$("li").click(function(event){
+		var link = event.toElement.getAttribute("href");
+		$("document").load(link);
+	});
+
+
 });
