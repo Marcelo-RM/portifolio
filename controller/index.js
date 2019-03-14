cf2f365cc5977244350532ecbfbe5d1daa494d52
marcelordrgs98@gmail.com <marcelordrgs98@gmail.com>
2019-03-14 12:01:15 +0000

    Movendo div (*precisa ajustar)

--- controller/index.js
+++ controller/index.js
@@ -1,8 +1,20 @@
-$(document).ready(function(){
-	$(".header").css('height', window.innerHeight);
+$(document).ready(function () {
+	$(".header").css("height", window.innerHeight);
 
-	$("li").click(function(event){
+	$("li").click(function (event) {
 		var link = event.toElement.getAttribute("href");
 		$("document").load(link);
+	});
+
+	$(window).scroll(function () {
+		var docWidth = $(window).width();
+		var divMargin = $(".delete").offset().left;
+		var divWidth = $(".delete").width();
+		var soma = divMargin + divWidth;
+		if(soma < docWidth){
+			$(".delete").css({
+				"marginLeft": ($(window).scrollTop()) + "px"
+			}, "slow");
+		}
 	});
 });
