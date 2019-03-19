3713bb25f5eafe6ca0b58f0ba9f1fe29f3d355f5
marcelordrgs98@gmail.com <marcelordrgs98@gmail.com>
2019-03-19 13:27:22 +0000

    adicionado movimento ao contrario

--- controller/index.js
+++ controller/index.js
@@ -1,7 +1,5 @@
-var lastScrollTop;
-
 $(document).ready(function () {
-	$(".header").css("height", window.innerHeight);
+	$(".header").css("height", $(window).innerHeight());
 
 	$("li").click(function (event) {
 		var link = event.toElement.getAttribute("href");
@@ -9,26 +7,27 @@ $(document).ready(function () {
 	});
 
 	$(window).scroll(function (event) {
-		var thisScrollTop = $(window).scrollTop(); 
+		var thisScrollTop = $(window).scrollTop();
 		var docWidth = $(window).width();
-		var divMarginLeft = $(".delete").offset().left;
-		var divWidth = $(".delete").width();
+		var divMarginLeft = $("#left").offset().left;
+		var divWidth = $("#left").width();
 		var soma = divMarginLeft + divWidth;
-		
-		if( lastScrollTop < thisScrollTop){
-			if(soma < docWidth){
-				$(".delete").css({
-					"marginLeft": ($(window).scrollTop()) + "px"
-				}, "slow");
-			}
+
+		if (soma < docWidth) {
+			$("#left").css({
+				"marginLeft": $(window).scrollTop() + "px"
+			}, "slow");
+			$("#right").css({
+				"marginRight": $(window).scrollTop() + "px"
+			}, "slow");
 		}
-		if(lastScrollTop > thisScrollTop){
-			if(divMarginLeft > 0 && thisScrollTop < (docWidth - divWidth)){
-				$(".delete").css({
-					"marginLeft": ($(window).scrollTop()) + "px"
-				}, "slow");
-			}
+		if (divMarginLeft > 0 && thisScrollTop < (docWidth - divWidth)) {
+			$("#left").css({
+				"marginLeft": $(window).scrollTop() + "px"
+			}, "slow");
+			$("#right").css({
+				"marginRight": $(window).scrollTop() + "px"
+			}, "slow");
 		}
-		lastScrollTop = thisScrollTop;
 	});
 });
