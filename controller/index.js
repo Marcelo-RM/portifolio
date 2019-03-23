9c7cad0c8c001128db9736a05f6e570985179d4b
Marcelo-RM <36489721+Marcelo-RM@users.noreply.github.com>
2019-03-23 09:44:00 -0300

    Adicionado busy

--- controller/index.js
+++ controller/index.js
@@ -3,17 +3,18 @@
 /**********************/
 
 $(document).ready(function () {
+	$("#busy").css("display", "none");
 	$(".header").css("height", $(window).innerHeight());
-	
+
 	/**
 	 * header list menu method
 	 * @event is the click event
 	 */
-	 
+
 	$("li").click(function (event) {
 		var link = event.toElement.getAttribute("href");
 		var text = event.toElement.innerText;
-	
+
 		if (text !== "MENU") {
 			$("html, body").animate({
 				scrollTop: $(link).offset().top
@@ -33,7 +34,7 @@ $(document).ready(function () {
  * Click in social medias in footer
  * @event is the clicked object
  */
- 
+
 function share(event){
 	var site = event.getAttribute('href');
 	window.open(site);
@@ -43,7 +44,7 @@ function share(event){
  * Click event in arrow down in header
  * @event is the link object
  */
- 
+
 function hideHeader(event){
 	var id = event.getAttribute('local');
 	$("html, body").animate({
@@ -55,14 +56,14 @@ function hideHeader(event){
  * This is a metho to move the div when the page is moved
  * @event is defined but never is used
  */
- 
+
 $(window).scroll(function (event) {
 	var thisScrollTop = $(window).scrollTop(),
 		docWidth = $(window).width(),
 		divMarginLeft = $("#left").offset().left,
 		divWidth = $("#left").width(),
 		soma = divMarginLeft + divWidth;
-	
+
 	if(thisScrollTop >= (docWidth - divWidth)){
 		$("#left").css({
 			"marginLeft": (docWidth - divWidth)
