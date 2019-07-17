3bca16bd53074d9737e509dce9bf0718cd9730e2
Marcelo Rodrigues Montalvão <marcelordrgs98@gmail.com>
2019-07-16 21:22:35 -0300

    create pwabuilder-sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,9 @@
-var staticChacheName = 'portifolio_16/07/2019_19:00';
-this.addEventListener("install", event => {
-    this.skipWaiting();
-
-    event.waitUntil(
-        caches.open(staticChacheName)
-        .then(cache => {
-            return cache.addAll([
-                '/'
-            ]);
-        })
-    );
-});
+if (navigator.serviceWorker.controller) {
+    console.log("[PWA BUILDER] active service worker found, no need to register");
+} else {
+    navigator.serviceWorker.register("pwabuilder-sw.js", {
+        scope: "./"
+    }).then(reg => {
+        console.log("service worker has been registred for scope: " + reg.scope);
+    })
+}
