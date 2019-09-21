fc6c496545f8f1f5c52645bf8b2cad4083fe7f24
Marcelo Rodrigues Montalvão <marcelordrgs98@gmail.com>
2019-09-21 12:42:03 -0300

    criação modal dinamico

--- controller/index.js
+++ controller/index.js
@@ -5,6 +5,20 @@
 $(document).ready(function() {
     //Prevent drag images on site
     $('img').on('dragstart', function(event) { event.preventDefault(); });
+
+    //Read json file to create list of courses
+    var success = function(data){
+        var output = '';  
+        $.each(data, function(key,val){
+          output += "<h5 class='w3-opacity'><b>"+ val.nome + '</b></h5>';
+          output += "<h6 class='w3-text-indigo'><i class='fa fa-calendar fa-fw w3-margin-right'></i>" + val.dataConclusao + "</h6>";
+          output += "<p>" + val.escola + "</p>";
+          output += "<hr class='styleHR'></hr>";
+        });
+        $('#listaCursos').html(output);
+    };
+
+    $.getJSON('../models/cursos.json', success);
 });
 
 /**********************/
@@ -20,3 +34,18 @@ function share(event) {
     var site = encodeURI(event.getAttribute('href'));
     window.open(site);
 }
+
+/**
+ * Metodo criado para exibir ou oculta model exibindo os cursos
+ * @event não será utilizado
+ */
+
+ function showHideModal(){
+    var modal = document.getElementById("cursos");
+
+    if(modal.style.display === "none" || modal.style.display === ""){
+        modal.style.display = "block";
+    } else {
+        modal.style.display = "none";
+    }
+ }
